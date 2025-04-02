1c3fb877a4a6444014b04ce85bef54e153682fdd
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-04-02 16:25:10 +0500

    Scaffolding...

---
+++ .nvim-personal/setup.lua
@@ -0,0 +1,32 @@
+local opts = { remap = false, silent = true }
+
+------------------------------------------------------------------------------------
+-- Кнопки работы с проектом.
+------------------------------------------------------------------------------------
+
+-- Space + A - Куча команд.
+tasks = {
+	{ "run", [[.venv\Scripts\ruff.exe check src && .venv\Scripts\python.exe src\main.py ]] },
+}
+
+vim.defer_fn(function()
+	keys = {}
+	for i, value in ipairs(tasks) do
+		table.insert(keys, i, value[1])
+	end
+
+	vim.keymap.set("n", "<leader>a", function()
+		require("fastaction").select(keys, {}, function(item)
+			for i, value in ipairs(tasks) do
+				if value[1] == item then
+					vim.g.hulvdan_task(item, value[2]):start()
+				end
+			end
+		end)
+	end, opts)
+end, 1)
+
+-- Вставить nocheckin, чтобы лишнего не вкоммичивать.
+vim.keymap.set("n", "<C-n>", function()
+	vim.api.nvim_input("o# nocheckin<ESC>")
+end, opts)
---
+++ README.md
@@ -0,0 +1,10 @@
+# Тестовое задание
+
+## Разработка
+
+```
+# Должен быть pyenv
+poetry install
+pre-commit install
+pre-commit install --install-hooks
+```
